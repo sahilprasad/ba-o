da616650bfecf17c35217d8c3408c7c4b3ebd269
James Baker <jbaker1@mit.edu>
2017-07-23 18:19:12 -0700

    Added firebase

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,54 @@
-var mongoose = require('mongoose');
+// var firebase = require('firebase');
+// import firebase from 'firebase';
 
-function clickDownstairsBathroom(event) {
-  // event.preventDefault();
-  console.log("downstairs bathroom clicked!");
+let downstairsBathroom = false
+let upstairsBathroom = false
+let washer = false
+let dryer = false
+
+firebase.database().ref(`/`)
+  .on('value', snapshot => {
+    downstairsBathroom = snapshot.val().downstairsBathroom
+    upstairsBathroom = snapshot.val().downstairsBathroom
+    washer = snapshot.val().washer
+    dryer = snapshot.val().dryer
+  }
+);
+
+function getStatus(room) {
+  switch (room) {
+    case 'downstairsBathroom':
+      return downstairsBathroom;
+    case 'upstairsBathroom':
+      return upstairsBathroom;
+    case 'washer':
+      return washer;
+    case 'dryer':
+      return dryer;
+  }
+}
+
+function updateDowstairsBathroomStatus() {
+  firebase.database().ref('/')
+    .set({ downstairsBathroom: !downstairsBathroom })
+}
+
+function updateUpstairsBathroomStatus() {
+  firebase.database().ref('/')
+    .set({ upstairsBathroom: !upstairsBathroom })
+}
+
+function updateWasherStatus() {
+  firebase.database().ref('/')
+    .set({ washer: !washer })
+}
+
+function updateDryerStatus() {
+  firebase.database().ref('/')
+    .set({ dryer: !dryer })
+}
+
+function clickDownstairsBathroom() {
+ // const { bathroom1 } = getDowstairsBathroomStatus()
+ console.log(getDownstairsBathroomStatus())
 }
